575905b8bda4732380f3334271a1d16a8d179282
AfterFm1 <aditrapking@gmail.com>
2024-07-05 14:40:48 +0530

    Update CategoryList.tsx

--- src/components/categoryList/CategoryList.tsx
+++ src/components/categoryList/CategoryList.tsx
@@ -3,7 +3,8 @@ import styles from "./categoryList.module.css"
 import Card from '../card/Card'
 
 const getData=async()=>{
-    const res=await fetch("http://localhost:3000/api/categories",
+  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+    const res=await fetch(`${apiBaseUrl}/api/categories`,
       {
         cache:"no-store"
       }
