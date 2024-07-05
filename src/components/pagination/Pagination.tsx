ade39d7362a85ee27e447db9cab7623c87b29a6f
AfterFm1 <aditrapking@gmail.com>
2024-07-05 15:16:19 +0530

    Update Pagination.tsx

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -11,36 +11,37 @@ import {
 } from "@/components/ui/pagination"
 
 const Paginate = ({page,lastPage,isBlog,cat}:{page:any,lastPage:any,isBlog:any,cat:any}) => {
+  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
   return (
     <div>
       <Pagination >
   <PaginationContent >
     {page!==1 &&
     <PaginationItem>
-      {cat && <PaginationPrevious href={`http://localhost:3000/blog?cat=${cat}&page=${page-1}`}/>}
-      {isBlog==="1" && !cat && <PaginationPrevious href={`http://localhost:3000/blog?page=${page-1}`}/>}
-      {isBlog!=="1" && <PaginationPrevious href={`http://localhost:3000?page=${page-1}`}/>}
+      {cat && <PaginationPrevious href={`${apiBaseUrl}/blog?cat=${cat}&page=${page-1}`}/>}
+      {isBlog==="1" && !cat && <PaginationPrevious href={`${apiBaseUrl}/blog?page=${page-1}`}/>}
+      {isBlog!=="1" && <PaginationPrevious href={`${apiBaseUrl}?page=${page-1}`}/>}
     </PaginationItem>}
     <PaginationItem>
-      {cat && <PaginationLink href={`http://localhost:3000/blog?cat=${cat}&page=1`}>1</PaginationLink>}
-      {isBlog==="1" && !cat && <PaginationLink href={`http://localhost:3000/blog?page=1`}>1</PaginationLink>}
-      {isBlog!=="1" && <PaginationLink href={`http://localhost:3000?page=1`}>1</PaginationLink>}
+      {cat && <PaginationLink href={`${apiBaseUrl}/blog?cat=${cat}&page=1`}>1</PaginationLink>}
+      {isBlog==="1" && !cat && <PaginationLink href={`${apiBaseUrl}/blog?page=1`}>1</PaginationLink>}
+      {isBlog!=="1" && <PaginationLink href={`${apiBaseUrl}?page=1`}>1</PaginationLink>}
     </PaginationItem>
     <PaginationItem>
-      {cat && <PaginationLink href={`http://localhost:3000/blog?cat=${cat}&page=2`}>2</PaginationLink>}
-      {isBlog==="1" && !cat && <PaginationLink href={`http://localhost:3000/blog?page=2`}>2</PaginationLink>}
-      {isBlog!=="1" && <PaginationLink href={`http://localhost:3000?page=2`}>2</PaginationLink>}
+      {cat && <PaginationLink href={`${apiBaseUrl}/blog?cat=${cat}&page=2`}>2</PaginationLink>}
+      {isBlog==="1" && !cat && <PaginationLink href={`${apiBaseUrl}/blog?page=2`}>2</PaginationLink>}
+      {isBlog!=="1" && <PaginationLink href={`${apiBaseUrl}?page=2`}>2</PaginationLink>}
     </PaginationItem>
     <PaginationItem>
-      {cat && <PaginationLink href={`http://localhost:3000/blog?cat=${cat}&page=3`}>3</PaginationLink>}
-      {isBlog==="1" && !cat && <PaginationLink href={`http://localhost:3000/blog?page=3`}>3</PaginationLink>}
-      {isBlog!=="1" && <PaginationLink href={`http://localhost:3000?page=3`}>3</PaginationLink>}
+      {cat && <PaginationLink href={`${apiBaseUrl}/blog?cat=${cat}&page=3`}>3</PaginationLink>}
+      {isBlog==="1" && !cat && <PaginationLink href={`${apiBaseUrl}/blog?page=3`}>3</PaginationLink>}
+      {isBlog!=="1" && <PaginationLink href={`${apiBaseUrl}?page=3`}>3</PaginationLink>}
     </PaginationItem>
     {page!==lastPage &&
     <PaginationItem>
-      {cat && <PaginationNext href={`http://localhost:3000/blog?cat=${cat}&page=${page+1}`} />}
-      {isBlog==="1" && !cat && <PaginationNext href={`http://localhost:3000/blog?page=${page+1}`} />}
-      {isBlog!=="1" && <PaginationNext href={`http://localhost:3000?page=${page+1}`} />}
+      {cat && <PaginationNext href={`${apiBaseUrl}/blog?cat=${cat}&page=${page+1}`} />}
+      {isBlog==="1" && !cat && <PaginationNext href={`${apiBaseUrl}/blog?page=${page+1}`} />}
+      {isBlog!=="1" && <PaginationNext href={`${apiBaseUrl}?page=${page+1}`} />}
     </PaginationItem>}
   </PaginationContent>
 </Pagination>
