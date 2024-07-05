1f6913aa11af45651784b287ebcf355e9c58abf9
AfterFm1 <aditrapking@gmail.com>
2024-07-05 08:39:35 +0530

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './blogPage.module.css'
 import CardList from '@/components/cardList/CardList'
 import Menu from '@/components/menu/Menu'
-const BlogPage = ({searchParams}) => {
+const BlogPage = ({searchParams}:{searchParams:any}) => {
   const page=parseInt(searchParams.page) || 1;
   const cat=searchParams.cat;
   const isBlog="1";
