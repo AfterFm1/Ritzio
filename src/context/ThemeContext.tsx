32426e4ade048b5142d8d70fdf7ec24bd5330b53
AfterFm1 <aditrapking@gmail.com>
2024-07-05 11:59:57 +0530

    Update ThemeContext.tsx

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,15 +1,19 @@
 "use client"
 import { useState, useEffect, createContext } from "react"
 
-export const ThemeContext=createContext();
+export const ThemeContext = createContext({
+    theme: "light",
+    toggle: () => {}
+  });
 
 const getFromLocalStorage=()=>{
     if(typeof window!=="undefined"){
         const value=localStorage.getItem("theme");
         return value || "light";
     }
+    return "light";
 }
-export const ThemeContextProvider=({children})=>{
+export const ThemeContextProvider=({children}:{children:any})=>{
     const [theme,setTheme]=useState(()=> {
         return getFromLocalStorage()}
     );
